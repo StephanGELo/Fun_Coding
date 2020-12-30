29970e17fa75b5d338a8dc5653220320a19848e8
thequinn <annasun.usa@gmail.com>
2020-12-30 10:12:56 -0800

    range(1, len(nums)) in Python

---
+++ anna/1480-Running_Sum_of_1d_Array.py
@@ -0,0 +1,12 @@
+'''
+1480. Running Sum of 1d Array
+'''
+
+class Solution:
+
+  def runningSum(self, nums: List[int]) -> List[int]:
+
+    for i in range(1, len(nums)):
+      nums[i] = nums[i-1] + nums[i]
+
+    return nums
